6eca45108d3cac21b54d0fd8c3cca5d3efe37686
Sebas <sebasverac331@gmail.com>
2024-10-29 16:15:33 -0600

    En el canva normal se añadió una función para mejorar el scroll haciendolo dinamico, y se paso el canva basico a Angular

--- Angular/Frontend/src/app/app.component.ts
+++ Angular/Frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CanvaComponent } from "./pages/canva/canva.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CanvaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- Angular/Frontend/src/app/app.routes.ts
+++ Angular/Frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { CanvaComponent } from './pages/canva/canva.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: CanvaComponent
+    },
+
+    {
+        path:'',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+];
